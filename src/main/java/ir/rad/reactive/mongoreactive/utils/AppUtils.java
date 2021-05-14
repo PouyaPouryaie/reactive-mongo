486ae018d32c0b6bf21406754a6f92a3d2393dfd
pouya <pouyapouryaie@gmail.com>
2021-05-14 15:51:45 +0430

    change mapper for test

--- src/main/java/ir/rad/reactive/mongoreactive/utils/AppUtils.java
+++ src/main/java/ir/rad/reactive/mongoreactive/utils/AppUtils.java
@@ -1,20 +1,20 @@
 package ir.rad.reactive.mongoreactive.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ir.rad.reactive.mongoreactive.dto.ProductDto;
 import ir.rad.reactive.mongoreactive.entity.Product;
 import org.springframework.beans.BeanUtils;
 
 public class AppUtils {
 
-    public static ProductDto entityToDto(Product source){
+    public static ProductDto entityToDto(Product source) {
         ProductDto destination = new ProductDto();
         BeanUtils.copyProperties(source, destination);
         return destination;
     }
 
-    public static Product dtoToEntity(ProductDto source){
-        Product destination = new Product();
-        BeanUtils.copyProperties(source, destination);
+    public static Product dtoToEntity(ProductDto source) {
+        Product destination = new ObjectMapper().convertValue(source, Product.class);
         return destination;
     }
 }
